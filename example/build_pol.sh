3558dd28a407b29beb924b2ba0aa2631d1e09e5e
tommason14 <tom.mason14@gmail.com>
2021-01-07 21:18:19 +1100

    build

--- example/build_pol.sh
+++ example/build_pol.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-fftool 400 ch.xyz 400 dhp.xyz 1116 swm4-ndp.xyz -b 70
+fftool 400 ch.xyz 400 dhp.xyz 1116 swm4-ndp.xyz -b 58
 packmol < pack.inp                                        
-fftool 400 ch.xyz 400 dhp.xyz 1116 swm4-ndp.xyz -b 70 -l
+fftool 400 ch.xyz 400 dhp.xyz 1116 swm4-ndp.xyz -b 58 -l
 polarizer -f cdrude.dff data.lmp pdata.lmp
 sed -i 's/0.000  # M/1e-16  # M/' pdata.lmp # python reads 1e-16 as 0 (massless site in swm4-ndp) 
 input_polariser in.lmp # adds coul/tt damping to any atom types containing H and O
