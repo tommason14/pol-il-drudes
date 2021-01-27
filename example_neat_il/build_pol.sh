3996cd16ec00c8c7f1c59e3b131d75f1934c818c
tommason14 <tom.mason14@gmail.com>
2021-01-27 15:47:18 +1100

    fixed example of neat IL

--- example_neat_il/build_pol.sh
+++ example_neat_il/build_pol.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-fftool 400 ch.xyz 400 dhp.xyz 1116 swm4-ndp.xyz -b 58
+fftool 200 ch.xyz 200 dhp.xyz -b 45
 packmol < pack.inp                                        
-fftool 400 ch.xyz 400 dhp.xyz 1116 swm4-ndp.xyz -b 58 -l
+fftool 200 ch.xyz 200 dhp.xyz -b 45 -l
 polarizer -f cdrude.dff data.lmp pdata.lmp
-sed -i 's/0.000  # M/1e-16  # M/' pdata.lmp # python reads 1e-16 as 0 (massless site in swm4-ndp) 
+gsed -i 's/0.000  # M/1e-16  # M/' pdata.lmp # python reads 1e-16 as 0 (massless site in swm4-ndp)  # sed on linux, not gsed
 input_polariser in.lmp # adds coul/tt damping to any atom types containing H and O
 pair_scaler
 
